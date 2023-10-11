c14e8a031193fd0dce17a26d052fb423f3ba7def
dyek18 <gjxodn000@naver.com>
2023-10-11 14:57:05 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// 예: 네비게이션 메뉴 스크롤링
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
